355fd2a3b0e2e0ce22caad77b3a80675f67d589d
Mahitha BYreddy <mahi.byreddy.975@gmail.com>
2019-12-12 18:02:58 -0800

    Checking in artifact code

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,24 +1,50 @@
-from __future__ import print_function
-import random
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+import config
 
-buzz = ('continuous testing', 'continuous integration',
-    'continuous deployment', 'continuous improvement', 'devops')
-adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
-adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
-    'seriously')
-verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
 
-def sample(l, n = 1):
-    result = random.sample(l, n)
-    if n == 1:
-        return result[0]
-    return result
+def construct_url(repo, path, name):
+    file_stats_path = 'storage/{}?stats'.format(repo + '/' + path + '/' + name)
+    return config.api_url.format(file_stats_path)
 
-def generate_buzz():
-    buzz_terms = sample(buzz, 2)
-    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
-        sample(verbs), buzz_terms[1]])
-    return phrase.title()
+
+def get_maven_artifacts():
+    data = 'items.find({"repo":{"$eq":"jcenter-cache"}})'
+    try:
+        r = requests.post(url=config.api_url.format('search/aql/'), data=data, auth=HTTPBasicAuth(config.username, config.password))
+    except Exception as e:
+        print "Exception {}".format(e)
+    results = json.loads(r.content)['results']
+    return results
+
+
+def get_file_stats_for_artifacts():
+    artifacts = get_maven_artifacts()
+    download_list = []
+    for item in artifacts:
+        new_url = construct_url(item['repo'], item['path'], item['name'])
+        try:
+            resp = requests.get(url=new_url, auth=HTTPBasicAuth(config.username, config.password), timeout=5)
+            content = json.loads(resp.content)
+        except Exception as e:
+            print "Exception {}".format(e)
+            continue
+        if content['downloadCount'] > 0:
+            download_list.append(content)
+    return download_list
+
+
+def get_most_popular_artifacts(artifacts_list):
+    sorted_list = sorted(artifacts_list, key=lambda i: (i.get('downloadCount'), i.get('lastDownloaded')), reverse=True)
+    return sorted_list[0], sorted_list[1]
+
+
+def main():
+    filtered_list = get_file_stats_for_artifacts()
+    first, second = get_most_popular_artifacts(filtered_list)
+    print first
+    print second
 
 if __name__ == "__main__":
-    print(generate_buzz())
+    main()
